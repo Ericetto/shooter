b1193fae41923ce08627a521a76d62d36f5b8a0e
Ericetto <erikimashev777@gmail.com>
2022-01-14 01:05:26 +0300

    add pistol aniamtions, fix animator

--- src/Hit Master/Assets/Code/Human/HumanAnimator.cs
+++ src/Hit Master/Assets/Code/Human/HumanAnimator.cs
@@ -8,12 +8,17 @@ namespace Code.Human
     {
         [SerializeField] private Animator _animator;
 
+        private readonly int _pistolTriggerParameterHash = Animator.StringToHash("Pistol");
+        private readonly int _rifleTriggerParameterHash = Animator.StringToHash("Rifle");
+
         private readonly int _runBoolParameterHash = Animator.StringToHash("Run");
         private readonly int _shootingBoolParameterHash = Animator.StringToHash("Shooting");
 
-        private readonly int _idleStateHash = Animator.StringToHash("Idle");
-        private readonly int _runStateHash = Animator.StringToHash("Run");
-        private readonly int _shootingStateHash = Animator.StringToHash("Shooting");
+        private readonly int _emptyStateHash = Animator.StringToHash("Empty");
+
+        private int _idleStateHash;
+        private int _runStateHash;
+        private int _shootingStateHash;
 
         private bool _canPlayShooting;
 
@@ -23,6 +28,19 @@ namespace Code.Human
 
         public event Action<AnimatorState> StateEntered;
         public event Action<AnimatorState> StateExited;
+
+        public void SetGunType(bool isPistol)
+        {
+            string gunType = isPistol ? "Pistol" : "Rifle";
+
+            _idleStateHash = Animator.StringToHash($"{gunType} Idle");
+            _runStateHash = Animator.StringToHash($"{gunType} Run");
+            _shootingStateHash = Animator.StringToHash($"{gunType} Shooting");
+            
+            _animator.SetTrigger(isPistol ? _pistolTriggerParameterHash : _rifleTriggerParameterHash);
+
+            _animator.Play(_emptyStateHash);
+        }
 
         public void EnteredState(int stateHash)
         {
@@ -34,14 +52,11 @@ namespace Code.Human
         {
             StateExited?.Invoke(StateFor(stateHash));
         }
-
-        public void SetActive(bool value) => _animator.enabled = value;
-
+        
         public void Run() => _animator.SetBool(_runBoolParameterHash, true);
-
         public void Stop() => _animator.SetBool(_runBoolParameterHash, false);
 
-        public void Shooting()
+        public void Shoot()
         {
             if (State != AnimatorState.Shooting)
             {
@@ -58,6 +73,8 @@ namespace Code.Human
         private void ShootingClipFinished() => _canPlayShooting = true;
 
         public void StopShooting() => _animator.SetBool(_shootingBoolParameterHash, false);
+
+        public void SetActive(bool value) => _animator.enabled = value;
 
         private AnimatorState StateFor(int stateHash)
         {
