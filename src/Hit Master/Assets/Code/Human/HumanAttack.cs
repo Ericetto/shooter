92e9ac50fc69f76b54b1d2eef4f92cb1952ecc9f
Ericetto <erikimashev777@gmail.com>
2022-01-13 21:20:43 +0300

    add ak47 model, rename model folders

--- src/Hit Master/Assets/Code/Human/HumanAttack.cs
+++ src/Hit Master/Assets/Code/Human/HumanAttack.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using Code.Weapon;
 
 namespace Code.Human
 {
@@ -7,14 +6,33 @@ namespace Code.Human
     public abstract class HumanAttack : MonoBehaviour
     {
         [SerializeField] protected HumanAnimator _animator;
-        [SerializeField] protected Gun _gun;
-        
+        [SerializeField] protected Transform _weaponHolder;
+
+        protected WeaponBase _weapon;
         protected bool _isActive;
+
+        public virtual void EquipWeapon(WeaponBase weapon)
+        {
+            _weapon = weapon;
+            _weapon.transform.SetParent(_weaponHolder);
+            _weapon.transform.localPosition = Vector3.zero;
+            _weapon.transform.localRotation = Quaternion.identity;
+            _weapon.transform.localScale = Vector3.one;
+        }
 
         public void Enable() => _isActive = true;
 
         public void Disable() => _isActive = false;
 
-        protected virtual void GunShoot() { }
+        // Calls from "Gun Shooting" animation clip
+        protected virtual void WeaponAttackByAnimation() { }
+
+        public virtual void DropWeapon()
+        {
+            _weapon.transform.SetParent(null);
+            _weapon.GetComponent<Collider>().enabled = true;
+            _weapon.SetActivePhysics(true);
+            _weapon.AddForce(new Vector3(0, 500, 0));
+        }
     }
 }
