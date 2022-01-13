c69078d844eb7f433ffb7454453386c7ca846c00
Ericetto <erikimashev777@gmail.com>
2022-01-13 03:59:34 +0300

    add bullet prefab with blood and environment hit fx

--- src/Hit Master/Assets/Code/Extensions/VectorExtensions.cs
+++ src/Hit Master/Assets/Code/Extensions/VectorExtensions.cs
@@ -4,24 +4,16 @@ namespace Code.Extensions
 {
     public static class VectorExtensions
     {
-        public static Vector3 AddX(this Vector3 vector, float x)
-        {
-            return new Vector3(vector.x + x, vector.y, vector.z);
-        }
+        public static Vector3 AddX(this Vector3 vector, float x) =>
+            new Vector3(vector.x + x, vector.y, vector.z);
 
-        public static Vector3 AddY(this Vector3 vector, float y)
-        {
-            return new Vector3(vector.x, vector.y + y, vector.z);
-        }
+        public static Vector3 AddY(this Vector3 vector, float y) =>
+            new Vector3(vector.x, vector.y + y, vector.z);
 
-        public static Vector3 WithX(this Vector3 vector, float x)
-        {
-            return new Vector3(x, vector.y, vector.z);
-        }
+        public static Vector3 WithX(this Vector3 vector, float x) =>
+            new Vector3(x, vector.y, vector.z);
 
-        public static Vector3 WithY(this Vector3 vector, float y)
-        {
-            return new Vector3(vector.x, y, vector.z);
-        }
+        public static Vector3 WithY(this Vector3 vector, float y) =>
+            new Vector3(vector.x, y, vector.z);
     }
 }
